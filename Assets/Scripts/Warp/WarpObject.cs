71b62e824c366261fb0e020dd59c87a35b11911e
qlop0000 <qlop0000@gmail.com>
2026-08-20 22:45:19 +0900

    새로운 지역 제작 및 개발용 방
    새로운 지역 : 골목 구현 (vision 기능 첫 사용 예시로 이용 예정)
    개발용 방 : 개발 편의성을 위한 warp 방 배치

--- Assets/Scripts/Warp/WarpObject.cs
+++ Assets/Scripts/Warp/WarpObject.cs
@@ -10,6 +10,9 @@ public class DirectWarpObject : InteractableObject
     public bool changeProgressOnWarp = false;
     public int nextProgressValue = 1;
 
+    [Header("워프 방식 설정")]
+    public bool warpOnTrigger = false;
+
     private WarpManager warpManager;
 
     void Start()
@@ -19,9 +22,23 @@ public class DirectWarpObject : InteractableObject
 
     public override void Interact()
     {
+        DoWarp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 워프 옵션 확인, 플레이어인지 확인
+        if (warpOnTrigger && collision.CompareTag("Player"))
+        {
+            DoWarp();
+        }
+    }
+
+    //warp 기능을 함수로 분리
+    private void DoWarp()
+    {
         if (warpManager != null)
         {
-            // 진도 제어 유무
             if (changeProgressOnWarp && ProgressManager.Instance != null)
             {
                 ProgressManager.Instance.SetProgress(nextProgressValue);
